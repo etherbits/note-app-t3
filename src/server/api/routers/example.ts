3e0628b865fd4e1e6d5a8ae166ce73104afe3563
etherbits <nika.qvrivishviliPC@gmail.com>
2023-03-03 17:08:59 +0400

    init

---
+++ src/server/api/routers/example.ts
@@ -0,0 +1,26 @@
+import { z } from "zod";
+
+import {
+  createTRPCRouter,
+  publicProcedure,
+  protectedProcedure,
+} from "~/server/api/trpc";
+
+export const exampleRouter = createTRPCRouter({
+  hello: publicProcedure
+    .input(z.object({ text: z.string(), rand: z.string() }))
+    .query(({ input }) => {
+      return {
+        greeting: `Hello ${input.text}`,
+        rand: `rand: ${input.rand}`
+      };
+    }),
+
+  getAll: publicProcedure.query(({ ctx }) => {
+    return ctx.prisma.example.findMany();
+  }),
+
+  getSecretMessage: protectedProcedure.query(() => {
+    return "you can now see this secret message!";
+  }),
+});
